e6704232bd1f80ad7e6a292d91d0c89bcbd304d5
Daniil Kaminskyi <deniil019@gmail.com>
2023-07-25 18:37:28 +0200

    Load on Server

--- Webstore_Red.com/Magaz/Volvo/sdfa.php
+++
@@ -1,8 +0,0 @@
-<?php
-// the message
-$msg = "First line of text\nSecond line of text";
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
-// send email
-echo mail("dhaurylkevich@gmail.com","My subject",$msg);
-?>
