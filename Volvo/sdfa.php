5068639a77a69d0bbc1c5cb68e3529ca54e11073
Daniil Kaminskyi <deniil019@gmail.com>
2023-07-25 22:17:09 +0200

    Load on Server

---
+++ Volvo/sdfa.php
@@ -0,0 +1,8 @@
+<?php
+// the message
+$msg = "First line of text\nSecond line of text";
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+// send email
+echo mail("dhaurylkevich@gmail.com","My subject",$msg);
+?>
---
+++ admin/update_users.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once '../config/config.php';
+
+$select = "SELECT * FROM users WHERE id = :id";
+
+$updateUserStmt = $dbh->prepare($select);
+$updateUserStmt->bindParam(':id', $_GET['id'], PDO::PARAM_STR);
+$updateUserStmt->execute();
+
+$user = $updateUserStmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Update User</title>
+    <link rel="stylesheet" href="../styles_elements/css/styles.css">
+    <link rel="stylesheet" href="../styles_elements/css/login.css">
+</head>
+<body>
+<a href="user_edit.php">
+    <img height="20px" style="padding: 10px; filter: invert(100%)" src="https://cdn.onlinewebfonts.com/svg/download_349454.png">
+</a>
+<form action="vendor/update_users.php" method="post">
+    <h3 style="color: white">Update User</h3>
+    <input type="text" name="id" value="<?= $user[0]['id'] ?>">
+    <p>Name</p>
+
+        <input type="text" name="username" value="<?= $user[0]['username'] ?>">
+
+    <p>Password</p>
+        <input type="password" name="password" value="<?= $user[0]['password'] ?>">
+    <p>Email</p>
+        <input type="email" name="email" value="<?= $user[0]['email'] ?>">
+    <br>
+        <button type="submit"> Update </button>
+    </br>
+</form>
+</body>
+</html>
